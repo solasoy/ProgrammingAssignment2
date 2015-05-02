ddf4e9dc289ac9226bbb4b63e02c913c5fbeba58
solasoy <solasoy2000@yahoo.com>
2015-05-02 16:28:17 -0500

    Signed-off-by: solasoy <solasoy2000@yahoo.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below demonstrate the use of scoping functions to calculate
+## the inverse of a matrix
 
-## Write a short comment describing this function
+
+## "makeCacheMatrix.R" returns a list of function calls for managing the cache that
+## stores a matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- solve(matrix())
+  
+  set <- function(y) {
+    f <- function(v) class(try(solve(v),silent=T))=="matrix"
+    if (f(y)==TRUE) {
+      x <<- y
+    } 
+    else {
+      stop("The input matrix is not invertible!")
+    }
+    
+    m <<- solve(matrix())
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve.R" returns the inverse of a matrix if it already exists in the cache and
+## determines the inverse of the matrix if it does not exist in the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  
+  if(!is.na(x$getinv())) {
+    message("getting cached data...")
+    m <- x$getinv()
+    return(m)
+  }
+  
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
